b2af18820bab08ab32980a9b4b44528e45e894fa
katherinehildreth <katherine.hildreth@gmail.com>
2025-12-18 23:14:15 -0600

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,16 @@
+const CACHE_NAME = 'gtfs-counter-v1';
+const FILES_TO_CACHE = [
+  './gtfs_counter_app_27a.html'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
