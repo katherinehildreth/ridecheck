5e0531013b1810679a5151ad2fb42c9f244cf052
katherinehildreth <katherine.hildreth@gmail.com>
2025-12-31 14:50:58 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,37 @@
-const CACHE_NAME = 'gtfs-counter-v1';
-const FILES_TO_CACHE = [
-  './gtfs_counter_app_27a.html'
+const CACHE_NAME = 'gtfs-counter-v2';
+const CORE_ASSETS = [
+  './',
+  './gtfs_counter_app_27a.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(CORE_ASSETS))
   );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k)))
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      caches.match('./gtfs_counter_app_27a.html')
+    );
+    return;
+  }
+
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request).then(res => res || fetch(event.request))
   );
 });
